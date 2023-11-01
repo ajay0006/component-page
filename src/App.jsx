3ffa774be357d3641d0651fd301272d8a6b7ba26
damilola <ajay0006@algonquinlive.com>
2023-11-01 10:28:40 -0400

    split app component into sub components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
+import './index.css'
+import {CORE_CONCEPTS} from "./data";
+import Header from "./components/Header/Header";
+import CoreConcepts from "./components/Core-concepts/Coreconcepts";
+
+
 function App() {
-  return (
-    <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <main>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        <CoreConcepts
+                            title={CORE_CONCEPTS[0].title}
+                            description={CORE_CONCEPTS[0].description}
+                            image={CORE_CONCEPTS[0].image}/>
+                        <CoreConcepts
+                            {...CORE_CONCEPTS[1]}
+                        />
+                        <CoreConcepts
+                            {...CORE_CONCEPTS[2]}
+                        />
+                        <CoreConcepts
+                            {...CORE_CONCEPTS[3]}
+                        />
+                    </ul>
+                </section>
+                <h2>Time to get started!</h2>
+            </main>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Core-concepts/Coreconcepts.jsx
@@ -0,0 +1,10 @@
+import './CoreConeptss.css'
+export default function CoreConcepts({image, title, description}) {
+    return (
+        <li>
+            <img src={image} alt={title}/>
+            <h3>{title}</h3>
+            <p>{description}</p>
+        </li>
+    )
+}
